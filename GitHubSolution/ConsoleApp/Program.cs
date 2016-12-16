b4504357ba9cb4bd3c531ccf159c4a5be1463a9f
Chaminda <Chaminda@DESKTOP-P1IVFFQ>
2016-12-17 01:35:54 +0530

    Commit to test CI

--- GitHubSolution/ConsoleApp/Program.cs
+++ GitHubSolution/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test1");
         }
     }
 }
